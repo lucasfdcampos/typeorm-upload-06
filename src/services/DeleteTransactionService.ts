16ab4c6608303dda25fcaea5a849e85778f3f850
lucasfdcampos <lucascampos.ads@gmail.com>
2020-06-28 19:07:44 -0300

    All tests OK.

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transacionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transacionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist.');
+    }
+
+    await transacionsRepository.remove(transaction);
   }
 }
 
